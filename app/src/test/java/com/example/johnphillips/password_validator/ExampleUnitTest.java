32f3b7e21801db63095b4f990dcdcd54c032dde5
John Phillips <jh355248@dal.ca>
2017-05-28 10:33:41 -0300

    a2-stage2

--- app/src/test/java/com/example/johnphillips/password_validator/ExampleUnitTest.java
+++ app/src/test/java/com/example/johnphillips/password_validator/ExampleUnitTest.java
@@ -37,4 +37,40 @@ public class ExampleUnitTest {
         assertTrue(passwordLong.length()>=8);
     }
 
+    //check to see if the password contains the word "password" at all
+    @Test
+    public void passwordContainsPassword() {
+        String passwordCaps = "PASSWORD";
+        String notPassword = "123456789";
+        String containsPassword = "1password2";
+
+        assertFalse(!passwordCaps.toLowerCase().contains("password"));
+        assertTrue(!notPassword.toLowerCase().contains("password"));
+        assertFalse(!containsPassword.toLowerCase().contains("password"));
+    }
+
+
+    //check to see if the password contains at least one number
+    @Test
+    public void passwordHasLetter(){
+        String passwordCaps = "PASSWORD";
+        String notPassword = "123456789";
+
+        assertFalse(passwordCaps.toLowerCase().contains("1")||passwordCaps.toLowerCase().contains("2")||passwordCaps.toLowerCase().contains("3")||passwordCaps.toLowerCase().contains("4")||passwordCaps.toLowerCase().contains("5")||passwordCaps.toLowerCase().contains("6")||passwordCaps.toLowerCase().contains("7")||passwordCaps.toLowerCase().contains("8")||passwordCaps.toLowerCase().contains("9")||passwordCaps.toLowerCase().contains("0"));
+        assertTrue(notPassword.toLowerCase().contains("1")||notPassword.toLowerCase().contains("2")||notPassword.toLowerCase().contains("3")||notPassword.toLowerCase().contains("4")||notPassword.toLowerCase().contains("5")||notPassword.toLowerCase().contains("6")||notPassword.toLowerCase().contains("7")||notPassword.toLowerCase().contains("8")||notPassword.toLowerCase().contains("9")||notPassword.toLowerCase().contains("0"));
+    }
+
+    //check to see if the password has an uppercase letter
+    @Test
+    public void passwordUppercase() {
+        String passwordCaps = "USAUSAUSA";
+        String passwordLower = "1australia2";
+        String passwordMixed = "SeAlSrCoOl";
+
+        assertFalse(!(passwordCaps.toLowerCase().equals(passwordCaps))&&!(passwordCaps.equals(passwordCaps.toUpperCase())));
+        assertFalse(!passwordLower.toLowerCase().equals(passwordLower)&&!passwordLower.toUpperCase().equals(passwordLower));
+        assertTrue(!passwordMixed.toLowerCase().equals(passwordMixed)&&!passwordMixed.toUpperCase().equals(passwordMixed));
+    }
+
+    
 }
